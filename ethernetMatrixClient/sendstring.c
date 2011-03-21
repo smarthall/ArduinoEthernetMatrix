5c78f444d520c78a3f21d5de6f767c7ed1862a7a
Daniel Hall <daniel@danielhall.me>
2011-03-22 09:44:29 +1100

    Started implementing the building of the memory image

--- ethernetMatrixClient/sendstring.c
+++ ethernetMatrixClient/sendstring.c
@@ -11,6 +11,24 @@
 
 #include "viewport.h"
 
+void bitblit(tuple ** dest, int destx, int desty, tuple **src, int srcx, int srcy, int width, int height) {
+  int maxx = srcx + width;
+  int maxy = srcy + height;
+
+  int xdiff = destx - srcx;
+  int ydiff = desty - srcy;
+
+  for (int x = srcx; x < maxx; x++) {
+    for (int y = srcy; y < maxy; y++) {
+      printf("(%d, %d) -> (%d, %d)\n", x, y, x + xdiff, y + ydiff);
+      //memcpy(dest[x + xdiff][y + ydiff], src[x][y], sizeof(sample) * 3);
+      dest[y + ydiff][x - xdiff][0] = src[y][x][0];
+      dest[y + ydiff][x - xdiff][1] = src[y][x][1];
+      dest[y + ydiff][x - xdiff][2] = src[y][x][2];
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
   FILE *pamfile = NULL;
@@ -36,6 +54,11 @@ int main(int argc, char *argv[])
   mempam.maxval = 255;
   strcpy(mempam.tuple_type, PAM_PPM_TUPLETYPE);
   memimage = pnm_allocpamarray(&mempam);
+  
+  for (int i = 0; i < textlen; i++) {
+    char ch = argv[2][i];
+    bitblit(memimage, i * 8, 0, fontimage, (ch - 32) * 8, 0, 8, 8);
+  }
 
   // Make a viewport
   display = allocviewport();
