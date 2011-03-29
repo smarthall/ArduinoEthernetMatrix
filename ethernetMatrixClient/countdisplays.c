f00e2a6195e901f99a91c57180c28be70e7f5793
Daniel Hall <daniel.hall@rea-group.com>
2011-03-30 08:16:32 +1100

    Now opens a socket to get the response

--- ethernetMatrixClient/countdisplays.c
+++ ethernetMatrixClient/countdisplays.c
@@ -22,14 +22,21 @@ void diep(char *s)
 
 int main(int argc, char *argv[])
 {
-  struct sockaddr_in si_other;
+  struct sockaddr_in si_other, si_me;
   int s, slen=sizeof(si_other);
   uint8_t data = 'C';
 
-
+  // Create a socket that uses UDP datagrams
   if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
     diep("socket");
 
+  // Address to listen on
+  memset((char *) &si_me, 0, sizeof(si_me));
+  si_me.sin_family = AF_INET;
+  si_me.sin_port = htons(PORT);
+  si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  // Machine we are sending to
   memset((char *) &si_other, 0, sizeof(si_other));
   si_other.sin_family = AF_INET;
   si_other.sin_port = htons(PORT);
@@ -38,9 +45,15 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
+  // Bind to the socket
+  if (bind(s,(struct sockaddr*) &si_me, sizeof(si_me))==-1)
+      diep("bind");
+
+  // Send the packet
   if (sendto(s, &data, sizeof(data), 0, (struct sockaddr *) &si_other, slen)==-1)
     diep("sendto()");
 
+  // Close the socket
   close(s);
   return 0;
 }
