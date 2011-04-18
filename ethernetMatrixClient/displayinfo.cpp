9b32dace466a5d75d00f24b332fcc0fcdf84a127
Daniel Hall <daniel@danielhall.me>
2011-04-18 20:55:43 +1000

    Display info

---
+++ ethernetMatrixClient/displayinfo.cpp
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "ethernetdisplay.h"
+
+#define PORT 1026
+#define SRV_IP "192.168.1.15"
+
+using namespace std;
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  try {
+    EthernetDisplay e = EthernetDisplay(SRV_IP, PORT);
+    
+    // Tell the user
+    printf("Geometry: (%d, %d)\n", e.getXSize(), e.getYSize());
+    printf("Displays: %d\n", e.getDisplayCount());
+    
+  } catch (const char *errmsg) {
+    std::cerr << "Ethernet Matrix Error: " << errmsg << "\n";
+    return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
+}
+
